3c7f95862c24f6c993f9446f83cb98d145425f23
TahaAlm <taha.elkihal@gmail.com>
2023-03-16 17:19:31 -0700

    C-hello_world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,14 @@
+#include <stdio.h>
 #include <unistd.h>
 
-int main(void) {
-  char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-  write(2, message, sizeof(message)-1); // -1 to exclude the null terminator
-  return 1;
+/**
+* main - Entry point
+*
+* Return: Always 1 (Success)
+*/
+int main(void)
+{
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
